cec0fdd7cd350e2079aff0c07d3cff2566875f72
tatiana <tatianachoque11@gmail.com>
2026-05-26 15:55:13 -0300

    configurando el gitignore para excluir carpeta env/

--- import.py
+++ import.py
@@ -1,4 +1,21 @@
-import pandas as pd
+import pandas as pd #importo pandas
 
 df=pd.read_csv("Vote_Ai.csv")
+#imprimo el archivo
 print("OKEY! Archivo cargado correctamente")
+#mostrar las primeras filas del dataframe
+print (df.head())# imprimo toda la tabla
+
+#filtra por año 2022
+resultado = df [df['Age']<50]
+print (resultado)
+#imprime las columnas
+#print(df.columns)
+#total_hadware = df['Postal_Vote_Ratio'].count()
+#print(f"Total de registros {total_hadware}")
+
+
+
+total_votos = df['Total_Votes'].sum()
+
+print(f"Total de votos: {total_votos}")
