e362121a7e6ec924709d859c69417738c36adbbe
tatiana <tatianachoque11@gmail.com>
2026-05-26 17:01:21 -0300

    actividad

--- import.py
+++ import.py
@@ -7,15 +7,25 @@ print("OKEY! Archivo cargado correctamente")
 print (df.head())# imprimo toda la tabla
 
 #filtra por año 2022
-resultado = df [df['Age']<50]
-print (resultado)
+#resultado = df [df['Age']<50]
+#print (resultado)
 #imprime las columnas
 #print(df.columns)
 #total_hadware = df['Postal_Vote_Ratio'].count()
 #print(f"Total de registros {total_hadware}")
 
+#total_votos = df['Total_Votes'].sum()
+#print(f"Total de votos: {total_votos}")
+
+print("---Analisis Avanzado de Datos---")
+ 
+filtro_avanzado = df['Attack_Type'].str.startswith('Cyber',na=False)
+df_filtro= df[filtro_avanzado]
+sumo_dinero=df_filtro['Campaign_Spending_Cr'].sum()
+print("--reporte financiero automatico--")
+print(f"monto total analizado:{sumo_dinero: .2f} millones. \n")
+filtro_numero=df['Campaign_Spending_Cr'] > 500
 
 
-total_votos = df['Total_Votes'].sum()
 
-print(f"Total de votos: {total_votos}")
+
